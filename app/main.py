4cb3afc695e4320bd8cf51ec723f06e77d2724d5
Gabriel Rodrigues <Gabrielrdriguesluna@gmail.com>
2026-06-14 21:05:32 -0300

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,15 +1,21 @@
 from fastapi import FastAPI
-from app.routes import alunos, disciplinas, matriculas, notas, health
 
-app = FastAPI(title="API Acadêmica")
-
-app.include_router(alunos.router, prefix="/v1/alunos")
-app.include_router(disciplinas.router, prefix="/v1/disciplinas")
-app.include_router(matriculas.router, prefix="/v1/matriculas")
-app.include_router(notas.router, prefix="/v1/notas")
-app.include_router(health.router, prefix="/v1/health")
-
+app = FastAPI(
+    title="API Gerenciamento Acadêmico"
+)
 
 @app.get("/")
-def root():
-    return {"message": "API rodando"}
+def home():
+    return {
+        "mensagem": "API Acadêmica funcionando com sucesso!"
+    }
+
+@app.get("/alunos")
+def listar_alunos():
+    return [
+        {
+            "id": 1,
+            "nome": "João Silva",
+            "email": "joao@email.com"
+        }
+    ]
