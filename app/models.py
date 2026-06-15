2101000719dd52afb9f10567986f19d1f00c4a79
Gabriel Rodrigues <Gabrielrdriguesluna@gmail.com>
2026-06-14 21:13:17 -0300

    Update models.py

--- app/models.py
+++ app/models.py
@@ -9,3 +9,11 @@ class Aluno(Base):
     email = Column(String, unique=True, index=True)
     matricula = Column(String, unique=True)
     periodo = Column(String)
+    
+    class Professor(Base):
+    __tablename__ = "professores"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nome = Column(String, index=True)
+    email = Column(String, unique=True, index=True)
+    especialidade = Column(String)
